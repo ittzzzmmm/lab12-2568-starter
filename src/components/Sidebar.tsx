55dff80f1aa56c4ebc17e72e939fe64e85f8cfb8
ittzzzmmm <ittzzzmmm@gmail.com>
2025-09-01 21:53:59 +0700

    ittzm

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -49,7 +49,7 @@ export default function Sidebar() {
               size="md"
               variant="default"
               radius="xl"
-              src="public\670610739 Sitiroj Hemlakpairoge.jpg"
+              src="https://scontent.fcnx4-2.fna.fbcdn.net/v/t39.30808-6/422910697_24564392106541641_7580726032199215984_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=K5S_qeng0aMQ7kNvwHmLGId&_nc_oc=AdlMIbPMNYAuKBR-k7PRY9gtwz610DlCTHYGyaHc2oxjAr_3G5l24A5AyP4OfB_XMeCzF7tlxrnGwFU0xJRTK3Oj&_nc_zt=23&_nc_ht=scontent.fcnx4-2.fna&_nc_gid=vEVMcA9xBQFC1Uuqlx_Ubw&oh=00_AfUiKbFn78jsLlItRbk5wroj2xN3sjqjU6GTPjDwTR8oog&oe=68BB89A3"
             />
           </Indicator>
           <Text>User : Sitiroj : Student</Text>
